3d684b27410a33dbf2d799505c952a2de848ba14
robert@binom.hr <robert@binom.hr>
2020-07-30 17:08:08 +0200

    Make two more API projects, Pricelist and ProductCatalog  and put the inside solution folder with simple dummy data

---
+++ ProductsAPIGateway/Pricelist/Controllers/PricelistController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Pricelist.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PricelistController : ControllerBase
+    {
+        private static readonly string[] Pricelist = new[]
+        {
+            "1001", "1002", "1003", "1004", "1005", "1006", "1007", "1008", "1009", "1010"
+        };
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Pricelist;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return Pricelist[id] ?? "There is no such price";
+        }
+    }
+}
---
+++ ProductsAPIGateway/ProductCatalog/Controllers/ProductsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ProductCatalog.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private static readonly string[] Products = new[]
+        {
+            "Samsung Galaxy A30", "Samsung Galaxy A50", "Samsung Galaxy A70", "Huawei P30", "Huawei P40", 
+        };
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return Products;
+        }
+
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return Products[id] ?? "There is no such product";
+        }
+    }
+}
